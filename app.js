330e122d6abf236e969fd68044214be8ecbe752d
ngocdiem26 <thachthingocdiem570@gmail.com>
2025-10-17 21:08:37 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,16 @@
+// app.js
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to contact book application." });
+});
+
+module.exports = app;
